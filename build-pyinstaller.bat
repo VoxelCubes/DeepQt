cadf74162dca9401d2839f4eba531a8b486f69aa
Voxel <41875513+VoxelCubes@users.noreply.github.com>
2023-04-03 03:08:17 +0100

    fix windows build path

--- build-pyinstaller.bat
+++ build-pyinstaller.bat
@@ -1,2 +1,2 @@
 :: Perform a Windows build.
-pyinstaller deepqt/main.py --onefile --noconfirm --clean --workpath=build --distpath=dist --windowed --name=DeepQt.exe --icon=media/logo.ico
+pyinstaller deepqt/main.py --onefile --noconfirm --clean --workpath=build --distpath=dist --windowed --name=DeepQt.exe --icon=media\logo.ico
